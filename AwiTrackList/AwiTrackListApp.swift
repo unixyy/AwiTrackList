227496bdc975724f98ec384d1fa6cfbf23296407
Richard MARTIN <rclic66000@gmail.com>
2023-02-07 10:01:35 +0100

    Initial Commit

---
+++ AwiTrackList/AwiTrackListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwiTrackListApp.swift
+//  AwiTrackList
+//
+//  Created by Richard Martin on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct AwiTrackListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
